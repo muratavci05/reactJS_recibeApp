36618546d3a5bf2fbf537731af663e9b9eb1f4b1
muratavci05 <js.muratavci@gmail.com>
2022-11-15 14:31:39 +0300

    app js api key,api id add and modified

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@
 function App() {
 
   const APP_ID = "bc2440e7";
+  const APP_KEY = "ed7d2e1bc9f2b2a57407fd01387a4eb8";
+ 
+ 
+  const exampleReg = 
+  `https://api.edamam.com/api/recipes/v2/0123456789abcdef0123456789abcdef?app_id=${APP_ID}&app_key=${APP_KEY}&type=public`;
 
   return (
     <div className="App">
