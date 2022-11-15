416662921d1bdb9943c3f6398abac532a27cc27d
muratavci05 <js.muratavci@gmail.com>
2022-11-15 16:06:39 +0300

    pages >> indexJS(regibe) add

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-function App() {
+import React from "react";
 
-  const APP_ID = "bc2440e7";
-  const APP_KEY = "ed7d2e1bc9f2b2a57407fd01387a4eb8";
- 
- 
-  const exampleReg = 
-  `https://api.edamam.com/api/recipes/v2/0123456789abcdef0123456789abcdef?app_id=${APP_ID}&app_key=${APP_KEY}&type=public`;
+import Regibe from "./Pages";
+
+function App() {
 
   return (
     <div className="App">
-      <div className="container" style={{width:"500px"}} >
-        <form className="form-control " style={{display:"flex",justifyContend:"center",marginTop:"20px"}}> 
-          <input className="form-control" type="text" />
-          <button className="btn btn-primary" type="submit">Search</button>
-        </form>
-      
-      </div>
-      
+     
+        <Regibe/>
     </div>
   );
 }
