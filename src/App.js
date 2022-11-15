5ec700077fb312b34ffc40a662ad438b36a195e5
muratavci05 <js.muratavci@gmail.com>
2022-11-15 13:46:15 +0300

    appjs modified

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+
+  const APP_ID = "bc2440e7";
+
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
