db2519f514e01250726bfdbc329f0459cbc7188d
muratavci05 <js.muratavci@gmail.com>
2022-11-16 19:43:26 +0300

    component files add

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from "react";
-import MealItem from "./Component/MealItem.js";
-import Recipe from "./Pages/Recibe.js";
+import React from 'react';
+import './Components/style.css';
+import Meal from './Components/Meal';
+import Recipe from './Components/Recipe';
+import {Route,Routes} from 'react-router-dom';
+
 function App() {
-
-
-
   return (
-    <div className="App">
-       <Recipe/>
-    </div>
-  );
+    <>
+   <Routes>
+      <Route  path="/" element={<Meal/>}/>
+      <Route exact path="/:recipeId" element={<Recipe/>}/>
+      </Routes>
+    </>
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+import {BrowserRouter} from "react-router-dom"
+ReactDOM.render(
+   < BrowserRouter>
     <App />
-  </React.StrictMode>
+    </BrowserRouter>  
+  ,
+  document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or end to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
