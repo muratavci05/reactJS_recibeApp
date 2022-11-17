76c15b24ed648c8783ed726945353991dfa0e789
muratavci05 <js.muratavci@gmail.com>
2022-11-17 13:06:38 +0300

    footer component file  modified

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -5,7 +5,9 @@ const Footer = () => {
     <div className="container footerComponent">
     <div className="container">
       <div className="card-header">Repice APP
-      <small><h6 style={{color:"gray"}}>Tüm Hakkı <span style={{color:"#404258"}}>Murat AVCI</span>'ya Aittir</h6></small></div>
+      <small><h6 style={{color:"gray"}}>Tüm Hakkı <span style={{color:"#404258"}}>Murat AVCI</span>'ya Aittir</h6>
+      <br/><h6><span style={{color:"gray"}} >contact:</span> js.muratavci@gmail.com</h6>
+      <h6><span style={{color:"gray"}} >Linkedin:</span> https://www.linkedin.com/in/muratavci05/</h6></small></div>
       <div className="card-body">
        
         <p className="card-text" style={{color:"gray"}}>
