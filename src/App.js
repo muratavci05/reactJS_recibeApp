5e083207c5a0daa0abf6efb61ff42347fe0722dc
muratavci05 <js.muratavci@gmail.com>
2022-11-17 12:39:27 +0300

    footer component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Meal from "./Components/Meal";
 import Recipe from "./Components/Recipe";
 import { Route, Routes } from "react-router-dom";
 import Header from "./Component/Header";
+import Footer from "./Components/footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Meal />} />
         <Route exact path="/:recipeId" element={<Recipe />} />
       </Routes>
+      <Footer/>
     </React.Fragment>
   );
 }
